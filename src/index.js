ebb705930440a8aac48c549e01c3b7782a516fc2
Cesar Moreno <cesar200526@gmail.com>
2018-03-27 00:14:37 -0500

    sass init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './libraries/Normalize.css'
+// import './index.css';
+// import App from './App';
+import Main from './modules/project-css/components/Main'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/modules/project-css/components/Main.jsx
@@ -0,0 +1,39 @@
+import React, {Component} from 'react'
+import '../sass/main.scss'
+
+class Main extends Component {
+  render () {
+    return (
+      <div>
+        <div className="main">
+          <div className="box" />
+        </div>
+        <div className="box2">
+          <div className="box2__title">name</div>
+          <div className="box2__link">
+            <a href="#">link</a>
+          </div>
+        </div>
+        <div className="box3">
+          <div className="widget"></div>
+          <div className="footer">
+            <div className="widget"></div>
+          </div>
+        </div>
+        <div className="box4">
+          <nav className="menu">
+            <ul>item 1</ul>
+            <ul>item 2</ul>
+            <ul>item 3</ul>
+            <ul>item 4</ul>
+            <ul>item 5</ul>
+          </nav>
+          <div className="btn">
+            <button className="btn">Click</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default Main
